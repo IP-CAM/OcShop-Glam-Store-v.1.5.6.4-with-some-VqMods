61af8291cb9d818c5b1114fcfa3f6517ef851bbd
ReJtm <l_y_n_x@list.ru>
2014-11-17 23:28:54 +0200

    Вернул старый файлменеджер

--- www/admin/view/javascript/ckeditor/config.js
+++ www/admin/view/javascript/ckeditor/config.js
@@ -7,15 +7,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For the complete reference:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-	config.filebrowserBrowseUrl = 'index.php?route=common/filemanager';
-	config.filebrowserImageBrowseUrl = 'index.php?route=common/filemanager';
-	config.filebrowserFlashBrowseUrl = 'index.php?route=common/filemanager';
-	config.filebrowserUploadUrl = 'index.php?route=common/filemanager';
-	config.filebrowserImageUploadUrl = 'index.php?route=common/filemanager';
-	config.filebrowserFlashUploadUrl = 'index.php?route=common/filemanager';		
-	config.filebrowserWindowWidth = '960';
-	config.filebrowserWindowHeight = '580';
-	
+	config.filebrowserWindowWidth = '800';
+	config.filebrowserWindowHeight = '500';
 	config.resize_enabled = false;
 	config.htmlEncodeOutput = false;
 	config.entities = false;
