2ff8532dd57b7fc43b8a103cf03004c5dc84ef89
ReJtm <l_y_n_x@list.ru>
2014-11-18 16:36:31 +0200

    Добавил модуль Таймер для акций
    Файл с изображением находится \image\data\special_price_countdown\

---
+++ www/admin/language/russian/module/special_price_countdown.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']       = 'Таймер для акционного товара';
+
+// Text
+$_['text_heading_title']  = 'Заголовок';
+$_['text_module']         = 'Модуль';
+$_['text_success']        = 'Success: You have modified module special price countdown!';
+$_['text_content_top']    = 'Верх страницы';
+$_['text_content_bottom'] = 'Низ страницы';
+$_['text_column_left']    = 'Левая колонка';
+$_['text_column_right']   = 'Правая колонка';
+
+
+// Entry
+$_['entry_layout']        = 'Страница:';
+$_['entry_position']      = 'Расположение:';
+$_['entry_status']        = 'Статус:';
+$_['entry_sort_order']    = 'Порядок сортировки:';
+$_['entry_digit_wh']      = 'Размер цифр (Ш X В)<span class="help">(ширина расчитывается на основе высоты)</span>';
+$_['entry_hours_bs']      = 'Отображать таймер за<span class="help"> X часов до окончания акции</span>';
+$_['entry_show_days']     = 'Отображать 00 дней<span class="help">(если осталось < 24 ч.)</span>';
+
+
+// Error
+$_['error_permission']    = 'Внимание: У вас нет прав для изменения настроек модуля!';
+?>
